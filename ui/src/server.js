2d193c13e9d0853dc494e114f671207af7609992
Kenneth Albinsson <kenneth.albinsson@conoa.se>
2023-02-20 14:48:48 +0100

    Update server.js

--- ui/src/server.js
+++ ui/src/server.js
@@ -1,17 +1,14 @@
 var express = require('express');
-var request=require('axios');
+var request=require('request');
 var mustacheExpress = require('mustache-express');
 var os = require('os');
-
 var app = express();
 app.set('view engine', 'html');
-app.engine('html', mustacheExpress());          // register file extension 
+app.engine('html', mustacheExpress());          // register file extension
 app.set('views', __dirname);
-
 app.get('/',function(req,res){
     res.status(200).send('Pets Demo Application');
 });
-
 app.get('/pet',function(req,res){
     var hostname = os.hostname();
     getPet(function(url){
@@ -21,14 +18,11 @@ app.get('/pet',function(req,res){
             });
     })
 });
-
 var getPet = function(callback) {
     request.get('http://api:8080/api/pet', function(err, res, body){
         url = JSON.parse(body).url;
         callback(url);
     });
 }
-
 app.listen(3000, '0.0.0.0');
-
 console.log("Listening at 0.0.0.0:3000");
